d0e15acc94d384ddf46635c0d5c1f0949f6fd4cf
YurenSUN <ysun299@wisc.edu>
2021-11-05 12:00:03 -0500

    update yuren's scripts

--- nn-methods/yuren/CHTC-scripts/cnn_rnn.py
+++ nn-methods/yuren/CHTC-scripts/cnn_rnn.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import gc
 import os
+import uuid
+import random
 
 from sklearn.metrics import precision_recall_curve, roc_curve
 from sklearn.model_selection import train_test_split
@@ -17,17 +19,25 @@ from tensorflow.keras.layers import GlobalMaxPooling1D, GlobalMaxPooling2D, MaxP
 from tensorflow.keras.layers import BatchNormalization, Dropout, Flatten, Input, Reshape
 
 
+# 1 - A, 2 - C, 3 - G, 4 - T, 0 - missing
+dic_complement = {0: 0, 1: 4, 2: 3, 3: 2, 4: 1}
+
+
 def read_xy(feature_path, response_path):
     """
     Read the input (x) file and label (y) file, parse x and y,
-    split into training, testing, and validation datsets.
-    Return the splitted data with their indices.
+    split into training, testing, and validation datsets,
+    augment the training dataset for true class with reverse complement,
+    down-dample the training dataset for false class to have a balanced
+    dataset, return the splitted data with their indices.
     @param feature_path: the file containing all the inputs (x)
     @param response_path: the file containing all the labels (y)
     """
     # Features, x
     feature_raw = h5py.File(feature_path, "r")
     features = np.array(feature_raw["features"])
+    # Remove the cols that all samples have the same site.
+    features = features[:, ~np.all(features[1:] == features[:-1], axis=0)]
 
     # Labels, y, 0 for false, 1 for True
     response_raw = pd.read_csv(response_path).dropna()
@@ -40,15 +50,57 @@ def read_xy(feature_path, response_path):
     for i in range(len(features)):
         x[i, range(len(features[i])), features[i]] = 1
 
-    # Covert true/false in y to 0 and 1
-    y = [1 if x else 0 for x in carb_tf]
+    # Covert true/false in y to 0/1
+    y = np.array([1 if x else 0 for x in carb_tf])
 
-    x_train, x_testVal, y_train, y_testVal, index_train, index_testVal = train_test_split(
-        x, y, range(len(x)), test_size=0.3)
-    x_test, x_val, y_test, y_val, index_test, index_val = train_test_split(
-        x_testVal, y_testVal, index_testVal, test_size=0.5)
+    # Split the indices seperately for 0/1
+    index_1 = np.argwhere(y == 1).flatten().tolist()
+    index_0 = np.argwhere(y == 0).flatten().tolist()
 
-    return x_train, y_train, x_val, y_val, x_test, y_test, index_test, index_val
+    # Split x and y
+    random.shuffle(index_1)
+    random.shuffle(index_0)
+    train_ratio = 0.7
+    test_val_ratio = 0.15
+
+    # Ratio for true:false class after augmentation
+    # Currently augment each sample to two with reverse complement
+    # Used only in training for a balanced dataset
+    # Use the whole dataset for val and test
+    ratio_1_0 = len(index_1) / len(index_0) * 2
+    index_0_shorten = index_0[:int(len(index_0)*ratio_1_0)]
+
+    train_index = index_1[:int(train_ratio * len(index_1))] + \
+        index_0_shorten[:int(train_ratio * len(index_0_shorten))]
+    test_index = index_1[int(train_ratio * len(index_1)):int((train_ratio+test_val_ratio) * len(index_1))] + \
+        index_0[int(train_ratio * len(index_0))
+                    :int((train_ratio+test_val_ratio) * len(index_0))]
+    val_index = index_1[int((train_ratio+test_val_ratio) * len(index_1)):] + \
+        index_0[int((train_ratio+test_val_ratio) * len(index_0)):]
+
+    print("train:", len(train_index), train_index)
+    print("val:", len(val_index), val_index)
+    print("test:", len(test_index), test_index)
+
+    x_train, y_train = x[train_index].tolist(), y[train_index].tolist()
+    x_test, y_test = x[test_index], y[test_index]
+    x_val, y_val = x[val_index], y[val_index]
+
+    # Augment the train index for true class (class 1) with reverse complement
+    # Add augment function if there for more augmentation methods later
+    train_index_1 = index_1[:int(train_ratio * len(index_1))]
+    features_train_1 = features[train_index_1]
+
+    for feature in features_train_1:
+        feature_new = [dic_complement[x] for x in np.flip(feature)]
+        # One hot encoding
+        feature_encode = np.zeros((len(feature_new), 5))
+        feature_encode[range(len(feature_new)), feature_new] = 1
+        # Append to trainig set
+        y_train += [1]  # class for true
+        x_train += [feature_encode]
+
+    return x_train, y_train, x_val, y_val, x_test, y_test, test_index, val_index
 
 
 def deepram_recurrent_onehot(x_shape, classes=2):
@@ -92,32 +144,42 @@ def deepram_conv1d_recurrent_onehot(x_shape, classes=2):
 
 
 if __name__ == "__main__":
+    # Some meta data
+    print("numpy version: ", np.__version__)
     print("tensorflow version: ", tf.__version__)
+    print("Num GPUs Available: ", len(
+        tf.config.experimental.list_physical_devices('GPU')))
+    print("physical devices: ", tf.config.list_physical_devices())
 
+    # Random str to store the output in case of conflict
+    random_str = str(uuid.uuid4())
     LOG_DIR = "./"
-    csv_path = LOG_DIR + 'DeepRam-dynamics.csv'
-    model_path = LOG_DIR + 'DeepRam.h5'
-    # Labels, y, 0 for false, 1 for True
+    csv_path = LOG_DIR + 'DeepRam-' + random_str + '-csv.csv'
+    model_path = LOG_DIR + 'DeepRam-' + random_str + '-h5.h5'
     typeUsed = ["false", "true"]
 
-    # Config tf to avoid InternalError
+    # Config tf for InternalError,
+    # Failed to call ThenRnnForward with model config:InternalError
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
     config.gpu_options.per_process_gpu_memory_fraction = 0.8
     tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
 
+    if tf.test.is_built_with_cuda():
+        print("The installed version of TensorFlow includes GPU support.")
+    else:
+        print("The installed version of TensorFlow does not include GPU support.")
+
     # Retrieve x and y
     x_train, y_train, x_val, y_val, x_test, y_test, index_test, index_val = read_xy(
         "features.jld", "responses.csv")
 
     # Compile model
-    # Modified from https://github.com/solislemuslab/dna-nn-theory/tree/master/cnn
     model = None
     keras.backend.clear_session()
     x_shape = (len(x_train[0]), len(x_train[0][0]))
-    model = deepram_recurrent_onehot(x_shape)
-    # model = deepram_conv1d_recurrent_onehot(x_shape)
+    model = deepram_conv1d_recurrent_onehot(x_shape)
     model.compile(optimizer='adam', loss='binary_crossentropy',
                   metrics='accuracy')
     model.summary()
@@ -128,20 +190,32 @@ if __name__ == "__main__":
         keras.callbacks.LambdaCallback(
             on_epoch_end=lambda epoch, logs: gc.collect(),
             # on_train_end=lambda logs: model.save(model_path)
-        )
+        ),
+        keras.callbacks.EarlyStopping(
+            monitor='val_loss', mode='min', verbose=1, patience=15)
     ]
 
-    history = model.fit(np.array(x_train), np.array(y_train), epochs=50, validation_data=(
-        np.array(x_val), np.array(y_val)), callbacks=callbacks, verbose=1, batch_size=4)
+    history = model.fit(np.array(x_train), np.array(y_train), epochs=100, validation_data=(
+        np.array(x_val), np.array(y_val)), callbacks=callbacks, verbose=2, batch_size=4)
 
     # print index of test and val for later reproduce on performance stat
     print("Index test: ", index_test)
     print("Index val: ", index_val)
+    print("y_test", y_test)
+    print("y_val", y_val)
+    print("Path:", csv_path, model_path)
+    
+    # Also store the indices and y value to csv file
+    with open(csv_path, "a") as f:
+        f.write(",".join(map(str, index_test)))
+        f.write(",".join(map(str, y_test)))
+        f.write(",".join(map(str, index_val)))
+        f.write(",".join(map(str, y_val)))
 
     # Evaluate
     model = keras.models.load_model(model_path)
-    results = model.evaluate(x=x_test, y=y_test)
-    y_score = model.predict(x_test)
+    results = model.evaluate(x=np.array(x_test), y=np.array(y_test))
+    y_score = model.predict(np.array(x_test))
     y_predit = [typeUsed[i]
                 for i in np.argmax(y_score, axis=1)]  # predicted class
     y_true = [typeUsed[i] for i in y_test]  # true class
--- nn-methods/yuren/readme.md
+++ nn-methods/yuren/readme.md
@@ -1,11 +1,58 @@
 ## Contents
 
-### Jupyter notebook files
+### Jupyter notebook file
 
-CNN-RNN.ipynb: the codes to read and parse the dataset, and define, compile, and train the model(s)
+CNN-RNN.ipynb: the codes to read and parse the dataset, and define, compile, and train the CNN and RNN models
+
+### CHTC-scripts folder
+
+Files to train the model on CHTC servers
+
+Possible OOM on some servers but could run on others.
 
 
 
-### CHTC folder
+## Models
 
-Files to train the model on CHTC Conder servers
+Models adapted from https://github.com/solislemuslab/dna-nn-theory/blob/master/cnn/dna_nn/model.py
+
+CNN
+
+- cnn_nguyen_conv1d_2_conv2d
+
+RNN
+
+- deepram_conv1d_recurrent_onehot
+
+- deepram_recurrent_onehot
+
+
+
+## Problem
+
+### Imbalanced dataset
+
+Carb: ~80% false, Toby: ~95% false. Model will biased to all the inputs as true.
+
+### Try to fix
+
+1. Current - down-sample for false class and augment the samples for true class with reverse complement to have a balanced training set
+
+
+
+## TODO
+
+### Data Augmentation
+
+- Currently using reverse complement to augment one more sample to each sample
+- Map the sequence to genome and shift to left or right. 
+  - Whole genome is not availabe
+- Extend half of the sequence and crop to use left, middle, and right
+  - Our sequences are too long, which would include too many manually generated data that are not similar to the original samples
+
+Searching for more methods
+
+### Change in training process
+
+- ? Set penalization in trainig process
+- ? Change the metric (not accuracy, maybe loss, ROC, or precision?) in optimizer when compile the model
